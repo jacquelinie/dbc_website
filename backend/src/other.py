1d7f8b703e799fbf968a9eeeec976d4c9e015223
Jacqueline <126740004+jacquelinie@users.noreply.github.com>
2023-09-01 14:03:29 +1000

    Added a test to signup

--- backend/src/other.py
+++ backend/src/other.py
@@ -7,3 +7,9 @@ Created: 28/8/2023
 Description: Contains helper functions if needed
 
 '''
+
+from src.config import url
+
+CLEAR_URL = f"{url}/clear"
+
+SIGNUP_URL = f"{url}/signup"
---
+++ backend/tests/signup_tests.py
@@ -0,0 +1,14 @@
+from src.config import port, url
+from src import other
+import requests
+import pytest
+
+@pytest.fixture
+def clear():
+    requests.delete(other.CLEAR_URL, json={})
+
+
+def test_signup(clear):
+    request_data = requests.post(other.SIGNUP_URL, json={'name': "Tester One", "email": "dbctesteremail@gmail.com"})
+    status = request_data.status_code
+    assert (status == 200) # Success
