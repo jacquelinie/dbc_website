f623943a095967b316ccc300fd244f14281fb20f
Jacqueline <126740004+jacquelinie@users.noreply.github.com>
2023-09-05 13:18:07 +1000

    Pushing tests

--- backend/src/payment.py
+++ backend/src/payment.py
@@ -11,7 +11,9 @@ Description: Handles the payment process for the customer
 import stripe
 from src.database import database
 
+stripe.api_key = "sk_test_51NmJ8xFpGEJAH0t4GRuvsDdwNXcfYnZgNyEnmN2yJqG0HvfLg4AUaddEBkSzrFPU0m2gebWSFICguxJIn3kQ55rd00HhStHZrX"
+
 def make_payment(customer_id, email, amount):
     store = database.get()
     users = store['users']
-    
+
