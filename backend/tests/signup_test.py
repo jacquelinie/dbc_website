63eec08a509484823b9df2b5e9fda2fa3f76628a
Jacqueline <126740004+jacquelinie@users.noreply.github.com>
2023-09-13 12:26:55 +1000

    Removed customer id

--- backend/tests/signup_test.py
+++ backend/tests/signup_test.py
@@ -11,6 +11,4 @@ def clear():
 def test_signup(clear):
     request_data = requests.post(other.SIGNUP_URL, json={'name': "Tester One", "email": "dbctesteremail@gmail.com"})
     status = request_data.status_code
-    customer_id = request_data.json()["customer_id"]
-    assert (customer_id == 0)
     assert (status == 200) # Success
