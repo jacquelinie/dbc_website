db5982f47c4f979c5081dc141d29931237b2597c
Jacqueline <126740004+jacquelinie@users.noreply.github.com>
2023-10-04 18:14:38 +1100

    Push time

---
+++ frontend_2/my-app/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import "./Home.css";
+
+const HomePage = (props) => {
+  return (
+    <div className="homepage">
+      <h2 className="large-text-white">Welcome to DBC</h2>
+
+      {/* Text Box */}
+      <div className="text-box">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+          sagittis vel tortor nec auctor.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
+
